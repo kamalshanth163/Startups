fec6ffd202aef77e0cb4d1f3c136f2f6b3f3a118
kamal-maverick <kamalshanthdeveloper163@gmail.com>
2024-01-05 10:48:06 +0530

    Add DataContext

---
+++ Startups.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Startups.Domain.Entities;
+
+namespace Startups.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Founder> Founders { get; set; }
+        public DbSet<Startup> Startups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Startup>()
+                .HasOne(b => b.Founder)
+                .WithMany(c => c.Startups)
+                .HasForeignKey(b => b.FounderId);
+
+            modelBuilder.Entity<Founder>()
+                .HasMany(c => c.Startups)
+                .WithOne(b => b.Founder)
+                .HasForeignKey(b => b.FounderId);
+        }
+    }
+}
+
