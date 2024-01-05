58c3267bd50a2d1ef2d78d8f7e42c5fade6aebc2
kamal-maverick <kamalshanthdeveloper163@gmail.com>
2024-01-05 10:42:35 +0530

    Add Controllers

---
+++ Startups.API/Controllers/FoundersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Startups.Application.Founders.Commands.RegisterFounder;
+using Startups.Application.Founders.Dtos;
+using Startups.Application.Founders.Queries.LoginFounder;
+using Startups.Domain.Entities;
+
+namespace Startups.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoundersController : BaseController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<Founder>> Register(RegisterFounderDto founder)
+        {
+            try
+            {
+                var registeredFounder = await Mediator.Send(new RegisterFounderCommand(founder));
+                return Ok(registeredFounder);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(LoginFounderDto founder)
+        {
+            try
+            {
+                var loggedFounder = await Mediator.Send(new LoginFounderQuery(founder));
+                return Ok(loggedFounder);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
+
+
---
+++ Startups.API/Controllers/StartupsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Startups.Application.Startups.Dtos;
+using Startups.Application.Startups.Commands.CreateStartup;
+using Startups.Application.Startups.Commands.DeleteStartup;
+using Startups.Application.Startups.Commands.UpdateStartup;
+using Startups.Application.Startups.Queries.GetStartupById;
+using Startups.Application.Startups.Queries.GetStartups;
+
+namespace Startups.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StartupsController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var startups = await Mediator.Send(new GetStartupsQuery());
+            return Ok(startups);
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var startup = await Mediator.Send(new GetStartupByIdQuery(id));
+
+            if (startup == null) NotFound();
+            return Ok(startup);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateStartupDto startup)
+        {
+            var createdStartup = await Mediator.Send(new CreateStartupCommand(startup));
+            return CreatedAtAction(nameof(GetById), new { id = createdStartup.Id }, createdStartup);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateStartupDto startup)
+        {
+            var updatedStartup = await Mediator.Send(new UpdateStartupCommand(startup));
+            return CreatedAtAction(nameof(GetById), new { id = updatedStartup.Id }, updatedStartup);
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var deletedStartupId = await Mediator.Send(new DeleteStartupCommand(id));
+            return Ok(deletedStartupId);
+        }
+    }
+}
